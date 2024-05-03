8e6c142863b20646610dd8de5a3ce2531d1e51c4
vipulranjansahu <vipulranjansahu@gmail.com>
2024-05-03 14:11:15 +0530

    Added node_modules to .gitignore

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const { connectToMongoDB } = require("./connect");
+const urlRoute = require("./routes/url");
+const URL = require("./models/url");
+const app = express();
+const PORT = 8001;
+
+connectToMongoDB("mongodb://localhost:27017/short-url").then(() =>
+  console.log("MongDB started")
+);
+
+app.use(express.json());
+
+app.use("/url", urlRoute);
+
+app.get("/:shortId", async (req, res) => {
+    const shortID = req.params.shortId;
+    const entry = await URL.findOneAndUpdate(
+      {
+        shortId: shortID,
+      },
+      {
+        $push: {
+          visitHistory: { timestamp: Date.now() },
+        },
+      }
+    );
+    res.redirect(entry.redirectURL);
+  });
+  
+
+app.listen(PORT, () => console.log("Server started at port", PORT));
